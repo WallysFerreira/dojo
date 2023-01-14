eb3bfd4e6c258ee65a239fe93304e3c42c59dd12
Wallys Ferreira <105322824+WallysFerreira@users.noreply.github.com>
2023-01-14 14:14:03 +0000

    adicionar algoritmo e testes criados do zero

--- 2-cheque-por-extenso/main.py
+++ 2-cheque-por-extenso/main.py
@@ -1,10 +1,21 @@
 def cheque(num):
-    lista1 = ["um", "dois", "tres", "quatro", "cinco",
-             "seis", "sete", "oito", "nove", "dez",
-             "onze", "doze", "treze", "quatorze", "quinze",
-             "dezesseis", "dezessete", "dezoito", "dezenove"]
+	unidade = ["um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove"]
+	dezenaDoDez = ["dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"]
+	dezena = ["vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
+	
+	numString = str(num)
 
-    lista2 = ["vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
+	if (len(numString) == 1):
+		return unidade[num - 1]
+	elif (len(numString) == 2):
+		if (num < 20):
+			return dezenaDoDez[num - 10]
+		else:
+			if (numString[1] == "0"):
+				index = int(numString[0])
+				return dezena[index - 2]
+			else:
+				index1 = int(numString[0])
+				index2 = int(numString[1])
 
-    return lista[num - 1]
-
+				return dezena[index1 - 2] + " e " + unidade[index2 - 1]
