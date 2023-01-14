0ea4c9586bf787d7f5728040b73aa14dd8f2f3ed
Wallys Ferreira <105322824+WallysFerreira@users.noreply.github.com>
2023-01-14 14:41:58 +0000

    corrigir identação

--- 2-cheque-por-extenso/main.py
+++ 2-cheque-por-extenso/main.py
@@ -1,22 +1,25 @@
 def cheque(num):
-	unidade = ["um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove"]
-	dezenaDoDez = ["dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"]
-	dezena = ["vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
-    
-	
-	numString = str(num)
+    unidade = ["um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove"]
+    dezenaDoDez = ["dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"]
+    dezena = ["vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
+    centena = ["cem", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"]
 
-	if (len(numString) == 1):
-		return unidade[num - 1]
-	elif (len(numString) == 2):
-		if (num < 20):
-			return dezenaDoDez[num - 10]
-		else:
-			if (numString[1] == "0"):
-				index = int(numString[0])
-				return dezena[index - 2]
-			else:
-				index1 = int(numString[0])
-				index2 = int(numString[1])
+    numString = str(num)
 
-				return dezena[index1 - 2] + " e " + unidade[index2 - 1]
+    if (len(numString) == 1):
+        return unidade[num - 1]
+    elif (len(numString) == 2):
+        if (num < 20):
+            return dezenaDoDez[num - 10]
+        else:
+            if (numString[1] == "0"):
+                index = int(numString[0])
+                return dezena[index - 2]
+            else:
+                index1 = int(numString[0])
+                index2 = int(numString[1])
+
+                return dezena[index1 - 2] + " e " + unidade[index2 - 1]
+    elif (len(numString) == 3):
+        if (num == 100):
+            return centena[0]
--- 2-cheque-por-extenso/test.py
+++ 2-cheque-por-extenso/test.py
@@ -3,22 +3,21 @@ import unittest
 from unittest import TestCase
 
 class Test(TestCase):
-	def teste_unidade(self):
-		self.assertEqual(cheque(1), "um")
-		self.assertEqual(cheque(9), "nove")
+    def teste_unidade(self):
+        self.assertEqual(cheque(1), "um")
+        self.assertEqual(cheque(9), "nove")
 
-	def teste_dezena_dez(self):
-		self.assertEqual(cheque(13), "treze")
-		self.assertEqual(cheque(18), "dezoito")
+    def teste_dezena_dez(self):
+        self.assertEqual(cheque(13), "treze")
+        self.assertEqual(cheque(18), "dezoito")
 
-	def teste_dezena(self):
-		self.assertEqual(cheque(60), "sessenta")
-		self.assertEqual(cheque(37), "trinta e sete")
-
+    def teste_dezena(self):
+        self.assertEqual(cheque(60), "sessenta")
+    
     def teste_centena(self):
         self.assertEqual(cheque(100), "cem")
         self.assertEqual(cheque(153), "cento e cinquenta e tres")
         self.assertEqual(cheque(644), "seiscentos e quarenta e quatro")
-		
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
