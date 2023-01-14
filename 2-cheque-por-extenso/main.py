475702d92b5ef6b4a98ca1dbbe5b1780d6bf1146
Wallys Ferreira <105322824+WallysFerreira@users.noreply.github.com>
2023-01-14 15:07:02 +0000

    melhorar teste e implementacao de centena

--- 2-cheque-por-extenso/main.py
+++ 2-cheque-por-extenso/main.py
@@ -7,31 +7,36 @@ def cheque(num):
     numString = str(num)
 
     if (len(numString) == 1):
-        return unidade[num - 1]
+        return unidade[num - 1] + " reais"
     elif (len(numString) == 2):
         if (num < 20):
-            return dezenaDoDez[num - 10]
+            return dezenaDoDez[num - 10] + " reais"
         else:
             if (numString[1] == "0"):
                 index = int(numString[0])
-                return dezena[index - 2]
+                return dezena[index - 2] + " reais"
             else:
                 index1 = int(numString[0])
                 index2 = int(numString[1])
 
-                return dezena[index1 - 2] + " e " + unidade[index2 - 1]
+                return dezena[index1 - 2] + " e " + unidade[index2 - 1] + " reais"
     elif (len(numString) == 3):
         if (num == 100):
-            return centena[0]
+            return centena[0] + " reais"
         elif (numString[2] == '0'):
             index1 = int(numString[0])
             index2 = int(numString[1])
 
-            return centena[index1] + " e " + dezena[index2 - 2]
+            return centena[index1] + " e " + dezena[index2 - 2] + " reais"
+        elif (numString[1] == '0'):
+            index1 = int(numString[0])
+            index2 = int(numString[2])
+
+            return centena[index1] + " e " + unidade[index2 - 1] + " reais"        
         else:
             index1 = int(numString[0])
             index2 = int(numString[1])
             index3 = int(numString[2])
             
-            return centena[index1] + " e " + dezena[index2 - 2] + " e " + unidade[index3 - 1]
+            return centena[index1] + " e " + dezena[index2 - 2] + " e " + unidade[index3 - 1] + " reais"
            
--- 2-cheque-por-extenso/test.py
+++ 2-cheque-por-extenso/test.py
@@ -4,20 +4,21 @@ from unittest import TestCase
 
 class Test(TestCase):
     def teste_unidade(self):
-        self.assertEqual(cheque(1), "um")
-        self.assertEqual(cheque(9), "nove")
+        self.assertEqual(cheque(1), "um reais")
+        self.assertEqual(cheque(9), "nove reais")
 
     def teste_dezena_dez(self):
-        self.assertEqual(cheque(13), "treze")
-        self.assertEqual(cheque(18), "dezoito")
+        self.assertEqual(cheque(13), "treze reais")
+        self.assertEqual(cheque(18), "dezoito reais")
 
     def teste_dezena(self):
-        self.assertEqual(cheque(60), "sessenta")
+        self.assertEqual(cheque(60), "sessenta reais")
     
     def teste_centena(self):
-        self.assertEqual(cheque(100), "cem")
-        self.assertEqual(cheque(153), "cento e cinquenta e tres")
-        self.assertEqual(cheque(640), "seiscentos e quarenta")
+        self.assertEqual(cheque(100), "cem reais")
+        self.assertEqual(cheque(153), "cento e cinquenta e tres reais")
+        self.assertEqual(cheque(640), "seiscentos e quarenta reais")
+        self.assertEqual(cheque(903), "novecentos e tres reais")
 
 if __name__ == '__main__':
     unittest.main()
